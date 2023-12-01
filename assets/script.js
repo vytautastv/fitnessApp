ed12b0c97ef2813e14b03e86655c2771a252f27c
vytautas <vytautastv98@gmail.com>
2023-12-01 18:18:51 +0000

    added api code

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,40 @@
-var muscle = "biceps";
-$.ajax({
-  method: "GET",
-  url: "https://api.api-ninjas.com/v1/exercises?muscle=" + muscle,
-  headers: { "X-Api-Key": "ZsgK8spWOxxkEn6A70+Svg==xtHt8idt25vLlexH" },
-  contentType: "application/json",
-  success: function (result) {
-    console.log(result);
-    workoutPlanner()
-},
-  error: function ajaxError(jqXHR) {
-    console.error("Error: ", jqXHR.responseText);
-  },
-});
-// put everyhting below in a function
+async function bmiCalulator() {
+    const url = 'https://bmi-calculator6.p.rapidapi.com/bmi?height=184&weight=86&system=metric';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '2bbff0e70bmsh8bceeebc7aa361dp110557jsn382a7ebb2ba6',
+            'X-RapidAPI-Host': 'bmi-calculator6.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.text();
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+bmiCalulator()
+//var muscle = "chest";
+//$.ajax({
+ // method: "GET",
+ // url: "https://api.api-ninjas.com/v1/exercises?muscle=" + muscle,
+ // headers: { "X-Api-Key": "ZsgK8spWOxxkEn6A70+Svg==xtHt8idt25vLlexH" },
+ // contentType: "application/json",
+ // success: function (result) {
+   //console.log(result);
+   // workoutPlanner()
+//},
+  //error: function ajaxError(jqXHR) {
+    //console.error("Error: ", jqXHR.responseText);
+  //},
+//});
+
 async function workoutPlanner() {
-    const url = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/back?limit=10';
+    const url = 'https://exercisedb.p.rapidapi.com/exercises/bodyPart/chest?limit=10';
     const options = {
         method: 'GET',
         headers: {
@@ -31,4 +51,5 @@ async function workoutPlanner() {
         console.error(error);
     }
 }
+workoutPlanner()
 
