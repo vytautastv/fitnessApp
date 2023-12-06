6a9cd4060041b71a51c13de7dc0e612744eed695
QS <qaleh.saleh@gmail.com>
2023-12-06 21:10:19 +0000

    Added changes to CSS HMTL

--- assets/script.js
+++ assets/script.js
@@ -55,27 +55,6 @@ $(document).ready(async function () {
     }
   }
 
-  async function workoutPlanner() {
-    const url =
-      "https://exercisedb.p.rapidapi.com/exercises/bodyPart/chest?limit=10";
-    const options = {
-      method: "GET",
-      headers: {
-        "X-RapidAPI-Key": "2bbff0e70bmsh8bceeebc7aa361dp110557jsn382a7ebb2ba6",
-        "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-      },
-    };
-
-    try {
-      const response = await fetch(url, options);
-      const result = await response.text();
-      console.log(result);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  workoutPlanner();
-
   // Code for storing the users inputs (email and name) to local storage
   function saveUserData() {
     // Get user inputs
